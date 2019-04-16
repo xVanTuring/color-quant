f9d34b1740bb3c21b2f16d9389ee22891363ba2a
xVanTuring <xVanTuring@outlook.com>
2019-04-16 23:53:48 +0800

    .gitignore:   * add ignore pattern for macOS LICENSE:   * update LICENSE colorquant.c:   * remove unused variables   * add LICENSE for leptonica package.json:  * update sharp.js version to latest

--- src/colorquant.c
+++ src/colorquant.c
@@ -1,3 +1,29 @@
+/*====================================================================*
+ -  Copyright (C) 2001-2016 Leptonica.  All rights reserved.
+ -
+ -  Redistribution and use in source and binary forms, with or without
+ -  modification, are permitted provided that the following conditions
+ -  are met:
+ -  1. Redistributions of source code must retain the above copyright
+ -     notice, this list of conditions and the following disclaimer.
+ -  2. Redistributions in binary form must reproduce the above
+ -     copyright notice, this list of conditions and the following
+ -     disclaimer in the documentation and/or other materials
+ -     provided with the distribution.
+ -
+ -  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ -  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ -  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ -  A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL ANY
+ -  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ -  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ -  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ -  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ -  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ -  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ -  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *====================================================================*/
+
 #include "colorquant.h"
 #include <math.h>
 #include <stdlib.h>
@@ -158,7 +184,6 @@ int *pix_median_cut_histo(PIX *pix, int sigbits, int sub_sample) {
     return NULL;
   }
   const int rshift = 8 - sigbits;
-  void *pixs = pix->pixs;
   const int num = pix->n;
   int index;
   for (int i = 0; i < num; i += sub_sample) {
@@ -191,7 +216,6 @@ BOX3D *pix_get_color_region(PIX *pix, int sigbits, int sub_sample) {
   int r_min = g_min = b_min = 1000000;
   int r_max = g_max = b_max = 0;
   const int rshift = 8 - sigbits;
-  int mask = 0xff >> rshift;
   const int num = pix->n;
   int r;
   int g;
